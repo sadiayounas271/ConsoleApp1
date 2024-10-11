ad28dfc839a378a4eb310e347ea9fa30953122d9
ASUS <ASUS@DESKTOP-FCA3NE1>
2024-10-11 08:10:37 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿string name = string.Empty;
+int age;
+
+Console.WriteLine("enter your name");
+name= Console.ReadLine();
+
+Console.WriteLine("Enter Age");
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Data {name}  {age}");
